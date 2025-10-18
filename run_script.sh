8a291aa1c47846508ebbcb36b51cc519f0576689
Lilygrace <lilygrace576@gmail.com>
2025-10-18 12:54:36 -0400

    revert

--- run_script.sh
+++ run_script.sh
@@ -3,5 +3,4 @@
 
 DATE=$1
 echo "Running for $DATE"
-# apptainer exec --bind /storage/osg-otte1/shared/TrinityDemonstrator:/mnt /storage/osg-otte1/shared/TrinityDemonstrator/DataAnalysis/containers/rootandexact.sif /mnt/DataAnalysis/DataSummary/FolderDataSum $DATE y
-apptainer exec --bind /storage/osg-otte1/shared/TrinityDemonstrator:/mnt /storage/osg-otte1/shared/TrinityDemonstrator/DataAnalysis/containers/rootandexact.sif /storage/osg-otte1/lsheram6/DataSummary/FolderDataSum $DATE y
+apptainer exec --bind /storage/osg-otte1/shared/TrinityDemonstrator:/mnt /storage/osg-otte1/shared/TrinityDemonstrator/DataAnalysis/containers/rootandexact.sif /mnt/DataAnalysis/DataSummary/FolderDataSum $DATE y
