0b257cb4499adfe7d9da324c5aa0fb2341d7d5dd
Lilygrace <lilygrace576@gmail.com>
2026-01-27 13:58:07 -0500

    Create LGSFolderDataSum.cpp

---
+++ LGSFolderDataSum.cpp
@@ -0,0 +1,88 @@
+#include "LGSDataSum.h"
+
+#include "rootils.h"
+#include "constants.h"
+
+#include <TCanvas.h>
+#include <TH2.h>
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int mainLG(int argc, char **argv){
+    if(argc != 3){
+        cout << "Usage: ./LGSFolderDataSum YYYYMMDD y/n" << endl;
+    }
+
+    std::string mount = argv[2];
+    
+    if (mount == "y"){ // with usingin htcondor you need to have contianers and some use full paths and other use mounts this lets you specify
+        std::cout << "using mounted directory path" << std::endl;
+        mnt="/mnt/";
+        dataDir = "/mnt/Data/";
+    }
+
+    string outStr = Form("%s%s",outDir.c_str(),argv[1]);
+    cout << "outStr" << outStr << endl;
+    LGDataSummary ds(argv[1]);
+    if(ds.hasData()){
+        ds.t_disp->Print((outStr+"LG.pdf[").c_str());
+
+        ds.PlotTrig();
+        ds.t_disp->Print((outStr+"LG.pdf").c_str());
+
+        ds.PlotROIMusic();
+        ds.t_disp->Print((outStr+"LG.pdf").c_str());
+
+        ds.PlotFF();
+        ds.t_disp->Print((outStr+"LG.pdf").c_str());
+        
+        ds.PlotHLED();
+        ds.t_disp->Print((outStr+"LG.pdf").c_str());  
+
+        ds.PlotHLEDNorm();
+        ds.t_disp->Print((outStr+"LG.pdf").c_str()); 
+    
+        ds.PlotPedestal();
+        ds.t_disp->Print((outStr+"LG.pdf").c_str());    
+
+        ds.PlotPedestalRMS();
+        ds.t_disp->Print((outStr+"LG.pdf").c_str()); 
+
+        ds.PlotAmplitude();
+        ds.t_disp->Print((outStr+"LG.pdf").c_str());   
+
+        ds.PlotCharge();
+        ds.t_disp->Print((outStr+"LG.pdf").c_str()); 
+
+        ds.PlotTimePeak();
+        ds.t_disp->Print((outStr+"LG.pdf").c_str());   
+        
+        ds.PlotPSF();
+        ds.t_disp->Print((outStr+"LG.pdf").c_str()); 
+
+        ds.t_disp->Print((outStr+"LG.pdf]").c_str());
+
+        ofstream csvOutput;
+        csvOutput.open((outStr+"LG.csv"));
+        csvOutput << ds.GetAvgEv() << "," << ds.GetAmpDist() << "," << ds.GetHLEDMean() << "," << ds.GetHLEDNMean() << "," << ds.GetPedMean() << "," << ds.GetPedRMSMean() << "," << ds.GetqMean() << "," << ds.GetPTMean() << "," << ds.GetPSFSigma();
+        for(vector<int>& vec : ds.GetTrTh()){
+            csvOutput << ",(" << vec[0] << "," << vec[1] << ")";
+        }
+        csvOutput.close();
+        
+        system(Form("chmod 660 %s",(outStr+"LG.pdf").c_str()));
+        system(Form("chmod 660 %s",(outStr+"LG.csv").c_str()));
+    }
+    else{
+        cout << "No data saved to Test branch on date " << argv[1] << endl;
+        ofstream csvOutput;
+        csvOutput.open((outStr+"LG.csv"));
+        csvOutput << "-1,-1,-1,-1,-1,-1,-1,-1,-1";
+        csvOutput.close();
+        system(Form("chmod 660 %s",(outStr+"LG.csv").c_str()));
+    }
+}
