6062a4106858d1f88156817aed54bd6834ac76f3
Lilygrace <lilygrace576@gmail.com>
2026-02-06 13:19:29 -0500

    Create DtStruct.cpp

---
+++ DtStruct.cpp
@@ -0,0 +1,46 @@
+//unchanged
+
+#include "DtStruct.h"
+
+#include "constants.h"
+
+#include <vector>
+
+#include <TROOT.h>
+
+using namespace std;
+
+DtStruct::DtStruct(bool isHLED) {
+    if(isHLED){
+        truHLED = true;
+        //changed from 2 to 4
+        data = vector<Double_t>(4,0.0);
+        pTrig = -1;
+    }
+    else{
+        truHLED = false;
+        //changed from 5 to 7?
+        data = vector<Double_t>(5,0.0);
+        pTrig = 0;
+    }
+    time = 0;
+}
+
+
+void DtStruct::Avg() {
+    if(truHLED){
+        for(size_t i = 0; i < data.size()-1; i++){
+            data[i] /= maxCh;
+        }
+    }
+    else{
+        for(size_t i = 0; i < data.size(); i++){
+            data[i] /= maxCh;
+        }
+    }
+}
+
+bool DtStruct::operator < (const DtStruct &other)
+{
+	return time < other.time;
+}
