ae942d7a220d60429b80e618fbe3e4064e272792
Lilygrace <lilygrace576@gmail.com>
2026-02-06 13:49:04 -0500

    Update DataSummary.h

--- DataSummary.h
+++ DataSummary.h
@@ -34,12 +34,14 @@ class DataSummary {
 		double ptMean;
 		double psfSigma;
 		vector<vector<int>> trTh;
-		//changed and split
+
+		//split
 		vector<DtStruct> testEv44;
 		vector<DtStruct> hledEv44;
 		vector<DtStruct> testEv41_5;
 		vector<DtStruct> hledEv41_5;
 		//
+
 		vector<vector<Double_t>> pixMeans;
 		vector<Double_t> meanPedRMS;
 		TF1 *fConvolutedFit;
@@ -54,6 +56,7 @@ class DataSummary {
 		TPaveText *pt;
 		bool isData;
 		void ReadEv(string readStr);
+
 		//changed to IEvent
 		bool isHLED(IEvent *&ev);
 		//changed and split
@@ -62,6 +65,7 @@ class DataSummary {
 		void AddTestEv41_5(IEvent *&ev);
 		void AddHLEDEv41_5(IEvent *&ev);
 		//
+
 		void ReadTrThresholds(string readStr);
 		void FillCamera(int dp);
 		void FillDt(int dp);
@@ -74,6 +78,7 @@ class DataSummary {
 		void PlotTrig();
 		void PlotROIMusic();
 		void PlotFF();
+
 		//split
 		void PlotHLED44();
 		void PlotHLEDNorm44();
@@ -82,6 +87,7 @@ class DataSummary {
 		void PlotAmplitude44();
 		void PlotCharge44();
 		void PlotTimePeak44();
+
 		void PlotHLED41_5();
 		void PlotHLEDNorm41_5();
 		void PlotPedestal41_5();
@@ -90,16 +96,29 @@ class DataSummary {
 		void PlotCharge41_5();
 		void PlotTimePeak41_5();
 		//
+
 		void PlotPSF();
 		vector<vector<int>> GetTrTh();
 		double GetAvgEv();
 		double GetAmpDist();
-		double GetHLEDMean();
-		double GetHLEDNMean();
-		double GetPedMean();
-		double GetPedRMSMean();
-		double GetqMean();
-		double GetPTMean();
+
+		//split
+		double GetHLEDMean44();
+		double GetHLEDNMean44();
+		double GetPedMean44();
+		double GetPedRMSMean44();
+		double GetqMean44();
+		double GetPTMean44();
+		double GetPSFSigma44();
+
+		double GetHLEDMean41_5();
+		double GetHLEDNMean41_5();
+		double GetPedMean41_5();
+		double GetPedRMSMean41_5();
+		double GetqMean41_5();
+		double GetPTMean41_5();
+		//
+
 		double GetPSFSigma();
 };
 
