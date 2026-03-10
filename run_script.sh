a9a9590f108b26fbf7f34aa3c27b13c6fa702544
Lilygrace <lilygrace576@gmail.com>
2026-03-10 12:35:18 -0400

    Delete run_script.sh

--- run_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# run_script.sh
-
-DATE=$1
-echo "Running for $DATE"
-apptainer exec --bind /home/lilyg/TrinityDemonstrator:/mnt /home/lilyg/TrinityDemonstrator/DataAnalysis/containers/rootandexact.sif /mnt/DataAnalysis/DataSummary/FolderDataSum $DATE y
