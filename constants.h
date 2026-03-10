d5ed35bc1225a02b5b9397798d71a370b1078a51
Lilygrace <lilygrace576@gmail.com>
2026-03-10 12:34:46 -0400

    Create constants.h

---
+++ constants.h
@@ -0,0 +1,19 @@
+#ifndef constants_H
+#define constants_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+extern string mnt;
+extern string dataDir;
+extern string outDir;
+extern const int maxCh;
+extern const int binLen;
+extern const vector<string> hTitles;
+extern const vector<string> dTitles;
+extern const vector<double> avgVals;
+extern const vector<int> MUSICmap;
+extern const string trStr;
+#endif
